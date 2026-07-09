13e61423ef2a8f59683f072d882af1a4207ffe3c
Jeff <bambamf16@gmail.com>
2026-07-09 15:55:24 -0500

    Add Home Assistant state access and Span breaker discovery

--- src/dynamic-entity-card.ts
+++ src/dynamic-entity-card.ts
@@ -3,6 +3,9 @@ import { customElement } from "lit/decorators.js";
 
 @customElement("dynamic-entity-card")
 export class DynamicEntityCard extends LitElement {
+  private config: any;
+  private _hass: any;
+
   static styles = css`
     ha-card {
       padding: 16px;
@@ -10,10 +13,31 @@ export class DynamicEntityCard extends LitElement {
     }
   `;
 
+  setConfig(config: any) {
+    this.config = config;
+  }
+
+  set hass(hass: any) {
+    this._hass = hass;
+  }
+
+  getCardSize() {
+    return 1;
+  }
+
   render() {
+    if (!this._hass) {
+      return html`Loading...`;
+    }
+
+    const breakers = Object.keys(this._hass.states)
+      .filter(entity => entity.startsWith("switch.span_"));
+
     return html`
       <ha-card>
         Dynamic Entity Card loaded
+        <br>
+        Found ${breakers.length} Span breakers
       </ha-card>
     `;
   }
